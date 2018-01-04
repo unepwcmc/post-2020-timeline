b83815d2a8cb6934d4732614b988001bccdf7b34
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2018-01-04 17:05:38 +0000

    Add Filter and FilterOption components

---
+++ app/javascript/components/filters/Filter.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="filter">
+
+    <p
+      @click="openSelect()" 
+      class="filter__button button" 
+      :class="{ 'filter__button--active' : isOpen , 'filter__button--has-selected' : hasSelected }">
+
+      {{ title }} <span v-show="hasSelected" class="filter__button-total">{{ totalSelectedOptions }}</span>
+    </p>
+
+    <ul class="filter__options ul-unstyled" :class="{ 'filter__options--active' : isOpen }">
+      <filter-option v-for="option in options" 
+        :option="option"
+        :selected="false">
+      </filter-option>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import { eventHub } from '../../home.js'
+  import FilterOption from './FilterOption.vue'
+
+  export default {
+    name: 'v-filter',
+
+    components: { FilterOption },
+
+    props: {
+      name: {
+        required: true,
+        type: String
+      },
+      title: {
+        required: true,
+        type: String
+      },
+      options: {
+        required: true,
+        type: Array
+      }
+    },
+
+    data () {
+      return {
+        isOpen: false
+      }
+    },
+
+    computed: {
+      selectedOptions () {
+        let selectedArray = []
+
+        // this.children.forEach(child => {
+        //   if(child.isSelected){ 
+        //     selectedArray.push(child.option) 
+        //   }
+        // })
+
+        return selectedArray
+      },
+
+      hasSelected () {
+        return this.totalSelectedOptions > 0
+      },
+
+      totalSelectedOptions () {
+        return this.selectedOptions.length
+      },
+    },
+
+    methods: {
+      openSelect () {
+        // if the filter is open is close it, else open it and close the others
+        if(this.isOpen){
+          this.isOpen = false
+        } else {
+          eventHub.$emit('clickDropdown', this.name)  
+        }
+      },
+
+      closeSelect () {
+        this.isOpen = false
+      },
+    }
+  }
+</script>
--- app/javascript/components/filters/Filters.vue
+++ app/javascript/components/filters/Filters.vue
@@ -1,9 +1,56 @@
 <template>
   <div class="filters">
     <h3>Filters</h3>
+
+    <v-filter v-for="filter in filters"
+      :name="filter.name"
+      :title="filter.title"
+      :options="filter.options"
+    >
+    </v-filter>
   </div>  
 </template>
 
 <script>
-  name: 'filters'
+  import VFilter from './Filter.vue'
+
+  export default {
+    name: 'filters',
+
+    components: { VFilter },
+
+    props: {
+      // filters: {
+      //   required: true,
+      //   type: Array
+      // }
+    },
+
+    data () {
+      return {
+        filters: [
+          {
+            name: 'category',
+            title: 'category',
+            options: ['category 1', 'category 2'],
+            multiple: false
+          },
+          {
+            name: 'organisers',
+            title: 'organisers',
+            options: ['organiser 1', 'organiser 2'],
+            multiple: true
+          }
+        ]
+      }
+    },
+
+    created () {
+      console.log('filters', this.filters)
+    },
+
+    methods: {
+
+    }
+  }
 </script>
