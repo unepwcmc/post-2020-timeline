5dd59a841490f7da1f34944e16b060c9c02e4827
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2018-01-08 12:07:35 +0000

    Add event vue component

---
+++ app/javascript/components/timeline/Event.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    {{ category }}
+    {{ title }}
+    {{ startDay }}
+    {{ endDay }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'event',
+
+    props: {
+      title: {
+        required: true,
+        type: String
+      },
+      category: {
+        type: String
+      },
+      startDay: {
+        required: true,
+        type: String
+      },
+      endDay: {
+        required: true,
+        type: String
+      }
+    }
+  }
+</script>
--- app/javascript/components/timeline/Month.vue
+++ app/javascript/components/timeline/Month.vue
@@ -3,14 +3,23 @@
     {{ month }}
 
     <div class="timeline__events">
-      
+      <event v-for="event in events"
+        :category="event.category"
+        :title="event.title"
+        :startDay="event.start_day"
+        :endDay="event.end_day"
+      ></event>
     </div>
   </div>
 </template>
 
 <script>
+  import Event from './Event.vue'
+
   export default {
     name: 'month',
+
+    components: { Event },
 
     props: {
       month: {
