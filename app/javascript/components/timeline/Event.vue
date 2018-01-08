9fa27166f3aa5d3dbd0eae25194cfc86f4c515c0
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2018-01-08 15:11:09 +0000

    Add event content and style

--- app/javascript/components/timeline/Event.vue
+++ app/javascript/components/timeline/Event.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    {{ category }}
-    {{ title }}
-    {{ startDay }}
-    {{ endDay }}
+  <div class="timeline__event">
+    <div class="timeline__event__date flex-center">
+      <span>{{ start }}</br>-</br>{{ end }}</span>
+    </div>
+
+    <span class="timeline__event__category">{{ category }}</span>
+    <span class="timeline__event__title">{{ title }}</span>
   </div>
 </template>
 
@@ -26,6 +28,37 @@
       endDay: {
         required: true,
         type: String
+      }
+    },
+
+    computed: {
+      start () {
+        return this.styledDate(this.startDay)
+      },
+
+      end () {
+        return this.styledDate(this.endDay)
+      }
+    },
+
+    methods: {
+      styledDate (day) {
+        let styled
+
+        if( day == '01' || day == '21' || day == '31') {
+          styled = day + 'st'
+
+        } else  if( day == '02' || day == '22') {
+          styled = day + 'nd'
+
+        } else  if( day == '03' || day == '23') {
+          styled = day + 'rd'
+
+        } else {
+          styled = day + 'th'
+        }
+
+        return styled
       }
     }
   }
