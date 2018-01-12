3debf5fcf7a6e4ceffcb832291c865d0d1d0fcfb
Andrew Potter <hello@drewpotter.com>
2018-01-12 13:48:06 +0000

    Removed filename parameter

--- app/models/event.rb
+++ app/models/event.rb
@@ -62,7 +62,7 @@ class Event < ApplicationRecord
     timeline.to_json
   end
 
-  def self.events_to_calendar(format = nil, filename)
+  def self.events_to_calendar(format = nil)
     events = Event.all
     calendar = Icalendar::Calendar.new
 
