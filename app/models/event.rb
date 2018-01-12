18d595c69c11a6570109b8bfac86e99d7941f486
Andrew Potter <hello@drewpotter.com>
2018-01-12 15:35:50 +0000

    Initial support for past_month and past_year in timeline json

--- app/models/event.rb
+++ app/models/event.rb
@@ -48,12 +48,14 @@ class Event < ApplicationRecord
       monthly_events = months.map do |month|
       {
         month: Date::ABBR_MONTHNAMES[month].downcase,
+        past_month: past_month(year_events, month),
         events: group_by_month(year_events, month, year)
       }
       end
 
       yearly_events_hash = {
         year: year,
+        past_year: past_year(year_events),
         months: monthly_events
       }
 
@@ -141,6 +143,19 @@ class Event < ApplicationRecord
 
     # We haven't found the current_event so we need to find the next event
     Event.where("start_date > ?", Date.today).order(start_date: :asc).first.id rescue nil
+  end
+
+  def self.past_month(year_events, month)
+    sql = %{
+      EXTRACT(month from start_date) = ? OR
+      EXTRACT(month from end_date) = ?
+    }
+
+    year_events.where(sql, month, month).where("end_date >= ?", Date.today).blank?
+  end
+
+  def self.past_year(year_events)
+    year_events.where("end_date >= ?", Date.today).blank?
   end
 
 end
