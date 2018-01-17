a9c7d44dcb6a0af3da40b60b0ac3cbad48a38e90
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2018-01-17 17:09:43 +0000

    Add hero modal

--- app/javascript/components/modal/Modal.vue
+++ app/javascript/components/modal/Modal.vue
@@ -1,44 +1,52 @@
 <template>
   <div class="modal" :class="{ 'modal--active' : isActive }" :style="{ 'top' : topPosition }">
-    
-    <div class="modal__title-box">
-      <p class="modal__category">{{ categories }}</p>
-      <h3>{{ modalContent.title }}</h3>
 
-      <button class="button button--close button--close-black modal__close" @click="closeModal"></button>
+    <button class="button button--close button--close-black modal__close" @click="closeModal"></button>
+
+    <div v-if="hero">
+      <slot></slot>
     </div>
     
-    <div class="modal__info-box">
-      <span class="flex flex-v-center">
-        <i class="icon icon--calendar modal__icon--calendar"></i>{{ modalContent.startDate }} - {{ modalContent.endDate }}
-      </span>
+    <div v-else>
+      <div class="modal__title-box">
+        <p class="modal__category">{{ categories }}</p>
+        <h3>{{ modalContent.title }}</h3>
+
+        <button class="button button--close button--close-black modal__close" @click="closeModal"></button>
+      </div>
       
-      <template v-if="hasContent(modalContent.location)">
+      <div class="modal__info-box">
         <span class="flex flex-v-center">
-          <i class="icon icon--pin modal__icon--pin"></i> {{ modalContent.location }}
+          <i class="icon icon--calendar modal__icon--calendar"></i>{{ modalContent.startDate }} - {{ modalContent.endDate }}
         </span>
+        
+        <template v-if="hasContent(modalContent.location)">
+          <span class="flex flex-v-center">
+            <i class="icon icon--pin modal__icon--pin"></i> {{ modalContent.location }}
+          </span>
+        </template>
+      </div>
+      
+      <template v-if="hasContent(modalContent.organisers)">
+        <h4>Organisers</h4>
+        <p>{{ modalContent.organisers }}</p>
+      </template>
+      
+      <template v-if="hasContent(modalContent.meetingSummary)">
+        <h4>Meeting summary</h4>
+        <p>{{ modalContent.meetingSummary }}</p>
+      </template>
+      
+      <template v-if="hasContent(modalContent.relevance)">
+        <h4>Relevance to post 2020</h4>
+        <p>{{ modalContent.relevance }}</p>
+      </template>
+      
+      <template v-if="hasContent(modalContent.outputs)">
+        <h4>Outputs</h4>
+        <p>{{ modalContent.outputs }}</p>
       </template>
     </div>
-    
-    <template v-if="hasContent(modalContent.organisers)">
-      <h4>Organisers</h4>
-      <p>{{ modalContent.organisers }}</p>
-    </template>
-    
-    <template v-if="hasContent(modalContent.meetingSummary)">
-      <h4>Meeting summary</h4>
-      <p>{{ modalContent.meetingSummary }}</p>
-    </template>
-    
-    <template v-if="hasContent(modalContent.relevance)">
-      <h4>Relevance to post 2020</h4>
-      <p>{{ modalContent.relevance }}</p>
-    </template>
-    
-    <template v-if="hasContent(modalContent.outputs)">
-      <h4>Outputs</h4>
-      <p>{{ modalContent.outputs }}</p>
-    </template>
   </div>
 </template>
 
@@ -55,15 +63,19 @@
         },
         isActive: false,
         modalContent: {},
-        top: 0
+        top: 0,
+        hero: true
       }
     },
 
     created () {
       eventHub.$on('openModal', this.openModal)
       eventHub.$on('closedModalWrapper', this.toggleModal)
+    },
 
-      this.updateModalContent()
+    mounted () {
+      // trigger modal and modal wrapper to open on page load
+      eventHub.$emit('openModal')
     },
 
     computed: {
@@ -77,11 +89,6 @@
     },
 
     methods: {
-      updateModalContent () {
-        // get the latest modal content
-        this.modalContent = this.$store.state.modal.modalContent
-      },
-
       toggleModal () {
         this.isActive = !this.isActive
       },
@@ -89,7 +96,10 @@
       openModal () {
         // get the y position and open the modal and modal wrapper
         this.getScrollY()
-        this.updateModalContent()
+        
+        // update the modal with the latest event content
+        if(!this.hero){ this.modalContent = this.$store.state.modal.modalContent }
+
         this.toggleModal()
       },
 
