fa242edc8f440829f8cb3206ef8affb400c12ad6
Stacy Talbot <stacymtalbot@gmail.com>
2018-03-23 11:11:29 +0000

    Remove / from the url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,6 +21,6 @@ class HomeController < ApplicationController
 
   private
     def allow_iframe
-      response.headers['X-Frame-Options'] = "ALLOW FROM https://www.cbd.int/"
+      response.headers['X-Frame-Options'] = "ALLOW FROM https://www.cbd.int"
     end
 end
