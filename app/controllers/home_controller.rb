bb9c2a7b26873fae719fd1bbc6ad92c92311b82f
Stacy Talbot <stacymtalbot@gmail.com>
2018-03-23 11:14:57 +0000

    Try the test page with the /test.html removed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,6 +21,6 @@ class HomeController < ApplicationController
 
   private
     def allow_iframe
-      response.headers['X-Frame-Options'] = "ALLOW FROM https://www.cbd.int"
+      response.headers['X-Frame-Options'] = "ALLOW FROM http://uknea.unep-wcmc.org"
     end
 end
