79fabe39632812ca2a2cdcb5bb1875704c66268c
Andrew Potter <hello@drewpotter.com>
2019-07-31 16:54:26 +0100

    Add more tests for the user accounts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "valid user should be correct" do
+  test "valid user should be valid" do
     user = FactoryBot.create(:user)
     assert user.valid?
   end
+
+  test "user without email should be invalid" do
+    user = FactoryBot.build(:user, email: "")
+    assert user.invalid?
+  end
+
+  test "user without password should be invalid" do
+    user = FactoryBot.build(:user, password: "")
+    assert user.invalid?
+  end  
 end
