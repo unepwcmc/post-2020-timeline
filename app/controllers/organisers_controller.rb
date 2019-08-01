f714a176acf9d891c80d5eea267804ca8c8d333d
Andrew Potter <hello@drewpotter.com>
2019-08-01 16:39:30 +0100

    Enforce authentication for categories and organisers

--- app/controllers/organisers_controller.rb
+++ app/controllers/organisers_controller.rb
@@ -1,4 +1,5 @@
 class OrganisersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_organiser, only: [:show, :edit, :update, :destroy]
 
   # GET /organisers
