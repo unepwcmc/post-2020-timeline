79e960ecfbb5cf63caca815633ae01ea1e9ec26e
Andrew Potter <hello@drewpotter.com>
2019-08-01 16:38:13 +0100

    Add menu for the newly created index pages for events, categories and organisers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -71,6 +71,6 @@ class EventsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
       params.require(:event).permit(:title, :start_date, :end_date, :location, :summary, :relevance, :outputs, :cbd_relation, :is_provisional_date,
-        :categories_attributes, :organisers_attributes)
+        categories_attributes: [{raw: []}, :raw, :category_id], organisers_attributes: [{raw: []}, :raw, :organiser_id])
     end
 end
