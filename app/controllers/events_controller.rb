73c5fa432529d457257c1207cf94611b496beece
Andrew Potter <hello@drewpotter.com>
2019-08-01 15:40:56 +0100

    Add categories and organisers to events form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -70,6 +70,7 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :start_date, :end_date, :location, :summary, :relevance, :outputs, :cbd_relation, :is_provisional_date)
+      params.require(:event).permit(:title, :start_date, :end_date, :location, :summary, :relevance, :outputs, :cbd_relation, :is_provisional_date,
+        :categories_attributes, :organisers_attributes)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event < ApplicationRecord
   has_and_belongs_to_many :categories, class_name: 'Category', join_table: 'event_categories'
   validates :title, presence: true
 
+  accepts_nested_attributes_for :organisers, :categories
+
   def self.filters_to_json
     events = Event.all.order(id: :asc)
     unique_categories = Category.pluck(:name).compact.uniq.sort
