43c1886749f8eac3c53874a783f1366ab83d9972
Andrew Potter <hello@drewpotter.com>
2019-08-01 14:52:46 +0100

    Disable recoverable with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :events
-  devise_for :users, skip: :registerable
+  devise_for :users, skip: [:registerable, :recoverable]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
