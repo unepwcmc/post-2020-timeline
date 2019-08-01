5e1c1f04dd0dba06c7280af65a2dd7723a063c5c
Andrew Potter <hello@drewpotter.com>
2019-08-01 10:51:57 +0100

    Add initial crud scaffold for the events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   devise_for :users, skip: :registerable
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
---
+++ test/controllers/events_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class EventsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @event = events(:one)
+  end
+
+  test "should get index" do
+    get events_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_event_url
+    assert_response :success
+  end
+
+  test "should create event" do
+    assert_difference('Event.count') do
+      post events_url, params: { event: { cbd_relation: @event.cbd_relation, end_date: @event.end_date, is_provisional_date: @event.is_provisional_date, location: @event.location, outputs: @event.outputs, relevance: @event.relevance, start_date: @event.start_date, summary: @event.summary, title: @event.title } }
+    end
+
+    assert_redirected_to event_url(Event.last)
+  end
+
+  test "should show event" do
+    get event_url(@event)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_event_url(@event)
+    assert_response :success
+  end
+
+  test "should update event" do
+    patch event_url(@event), params: { event: { cbd_relation: @event.cbd_relation, end_date: @event.end_date, is_provisional_date: @event.is_provisional_date, location: @event.location, outputs: @event.outputs, relevance: @event.relevance, start_date: @event.start_date, summary: @event.summary, title: @event.title } }
+    assert_redirected_to event_url(@event)
+  end
+
+  test "should destroy event" do
+    assert_difference('Event.count', -1) do
+      delete event_url(@event)
+    end
+
+    assert_redirected_to events_url
+  end
+end
