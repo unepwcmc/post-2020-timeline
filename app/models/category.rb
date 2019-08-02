5394e46f48c2061fc32e2a228008c5b881af7acb
Andrew Potter <hello@drewpotter.com>
2019-08-02 14:05:33 +0100

    Add validations to models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :events, class_name: "Event", join_table: 'event_categories'
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
