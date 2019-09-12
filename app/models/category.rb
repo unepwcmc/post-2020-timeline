9cdfedb10512146690f2b8732bfbbb2c05334351
Andrew Potter <hello@drewpotter.com>
2019-09-12 10:36:45 +0100

    Add tests for category model and refactor category model validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :events, class_name: "Event", join_table: 'event_categories'
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "category name has to be unique" do
+    category1 = FactoryBot.create(:category, name: "Event 1")
+    category2 = FactoryBot.build(:category, name: "Event 1")
+    assert_not category2.valid?
+  end
+
+  test "category name cannot be blank" do
+    category = FactoryBot.build(:category, name: "  ")
+    assert_not category.valid?
+  end
+
+  test "category name cannot be empty" do
+    category = FactoryBot.build(:category, name: "")
+    assert_not category.valid?
+  end
 end
