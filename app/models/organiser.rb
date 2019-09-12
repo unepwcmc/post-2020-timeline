6bcdab16cd54f5cd83fd3a458ac11026ce58aa1a
Andrew Potter <hello@drewpotter.com>
2019-09-12 10:32:53 +0100

    Add tests for organiser model and improve validations

--- app/models/organiser.rb
+++ app/models/organiser.rb
@@ -1,4 +1,4 @@
 class Organiser < ApplicationRecord
   has_and_belongs_to_many :events, class_name: "Event", join_table: 'event_organisers'
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true, allow_blank: false
 end
